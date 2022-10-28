0541a0f453a4e9701a25f1194dc5d9899aa22a3a
AJ Timek <Timek612@gmail.com>
2022-10-28 15:09:16 -0500

    added custom font. working on some flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+
       <div className='parallax'>
-      <Header />
-      <div id='dov'>
-        Scroll Up and Down this page to see the parallax scrolling effect.
-        This div is just here to enable scrolling.
-        Tip: Try to remove the background-attachment property to remove the scrolling effect.
+        <Header />
+        <div id='welcomeDiv'>
+          <h1 id='welcomeHeader'>
+            Hi, I'm AJ
+          </h1>
+
+          <h2 id='subHeader'>
+            Welcome to my portfolio :)
+          </h2>
+        </div>
+        <div id='dov'>
+          <h1>A little about me</h1>
+          <section>
+            <div id='flexDiv'>
+              <div id='myFace'>
+
+              </div>
+              <div id='aboutMeText'>
+                <p>My name is AJ Timek, I am a 22-year old Web Developer</p>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
-      </div>
-      
+
     </div>
   );
 }
