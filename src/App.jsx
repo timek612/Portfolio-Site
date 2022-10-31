d9e5c7576ccfe9164e38a5baa706b0f90519f33c
AJ Timek <Timek612@gmail.com>
2022-10-31 12:26:04 -0500

    added more about info and organized code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from './Header/Header';
+import About from './About';
 import './App.css';
 
 function App() {
@@ -17,26 +18,7 @@ function App() {
           </h2>
         </div>
         <div id='dov'>
-          <h1>A little about me</h1>
-          <section id='IntroSection'>
-            <div className='flexDiv'>
-              <div id='myFace'></div>
-              <div className='aboutText'>
-                <p>My name is AJ Timek, I am a 22-year old Web Developer with a passion for creating projects that can make an impact.</p>
-              </div>
-            </div>
-          </section>
-          <br />
-          <section id='primeSection'>
-            <div className='flexDiv'>
-              <div className='aboutText'>
-                  <p>I recently transitioned to the path of a Web Developer by going through Prime 
-                    Digital Academy's course in 2022.
-                  </p>
-              </div>
-              <div id='primeLogo'></div>
-            </div>
-          </section>
+          <About />
         </div>
       </div>
 
