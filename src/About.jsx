a2e23693d457e04e95e40cee8ddd46ee650c4364
AJ Timek <Timek612@gmail.com>
2022-10-31 14:06:18 -0500

    working on displaying videos of my projects

--- src/About.jsx
+++ src/About.jsx
@@ -1,53 +1,53 @@
 function About() {
     return (
         <>
-        <h1>A little about me</h1>
-          <section id='IntroSection'>
-            <div className='flexDiv'>
-              <div id='myFace'></div>
-              <div className='aboutText'>
-                <p>My name is AJ Timek, I am a 22-year old Web Developer with a passion for creating projects that can make an impact.</p>
-              </div>
-            </div>
-          </section>
-          <br />
-          <section id='primeSection'>
-            <div className='flexDiv'>
-              <div className='aboutText'>
-                  <p>I recently transitioned to the path of a Web Developer by going through Prime 
-                    Digital Academy's course in 2022.
-                  </p>
-              </div>
-              <div id='primeLogo'></div>
-            </div>
-          </section>
-          <br />
-          <section id='proficiencies'>
-            <div className='flexDiv'>
-                <div id='reactLogo'></div>
-                <div className='aboutText'>
-                  <p>
-                    My proficient skills: JavaScript, React.js, Node.js, 
-                    Express.js, PostgreSQL, REST Api's, Redux/Redux-saga, Git, and others.
-                  </p>
+            <h1>A little about me</h1>
+            <section id='IntroSection'>
+                <div className='flexDiv'>
+                    <div id='myFace'></div>
+                    <div className='aboutText'>
+                        <p>My name is AJ Timek, I am a 22-year old Web Developer with a passion for creating projects that can make an impact.</p>
+                    </div>
                 </div>
-            </div>
-          </section>
-          <br />
-          <section>
-            <div className='flexDiv'>
-              <div className='aboutText'>
-                  <p>
-                    In my free time, I love rock climbing, disc golfing, playing video games,
-                    and enjoying the company of my friends!
-                  </p>
-              </div>
-              <div id='climbingPic'></div>
-            </div>
-          </section>
-          <br />
-          <hr />
-          </>
+            </section>
+            <br />
+            <section id='primeSection'>
+                <div className='flexDiv'>
+                    <div className='aboutText'>
+                        <p>I recently transitioned to the path of a Web Developer by going through Prime
+                            Digital Academy's course in 2022.
+                        </p>
+                    </div>
+                    <div id='primeLogo'></div>
+                </div>
+            </section>
+            <br />
+            <section id='proficiencies'>
+                <div className='flexDiv'>
+                    <div id='reactLogo'></div>
+                    <div className='aboutText'>
+                        <p>
+                            My proficient skills: JavaScript, React.js, Node.js,
+                            Express.js, PostgreSQL, REST Api's, Redux/Redux-saga, Git, and others.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <br />
+            <section>
+                <div className='flexDiv'>
+                    <div className='aboutText'>
+                        <p>
+                            In my free time, I love rock climbing, disc golfing, playing video games,
+                            and enjoying the company of my friends!
+                        </p>
+                    </div>
+                    <div id='climbingPic'></div>
+                </div>
+            </section>
+            <br />
+            <hr />
+        </>
     )
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,31 @@ function App() {
         </div>
         <div id='dov'>
           <About />
+          <h1>Projects</h1>
+          <p>Throughout my time at Prime Digital Academy, I created numerous apps. While we started out with just
+            front-end apps, full stack came shortly after.
+          </p>
+          <br />
+          <section id='salaryCalculator'>
+            <div className='flexDiv'>
+              <div className='aboutText'>
+                <p>
+                  My first weekend challenge: A jQuery company salary calculator. An app to calculate company monthly employee costs.
+                </p>
+              </div>
+              <div className='video'><iframe width={500} height={300} src="https://player.vimeo.com/video/765809707?h=aa507c2885&amp;badge=0&amp;loop=1autopause=0&amp;player_id=0&amp;app_id=58479"
+                frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen
+                title="jquery Salary Calculator"></iframe>
+              </div>
+            </div>
+          </section>
+          <br />
+          <section id='serverSideCalculator'>
+            <div className='flexDiv'>
+              <div className='video'></div>
+              <div className='aboutText'></div>
+            </div>
+          </section>
         </div>
       </div>
 
